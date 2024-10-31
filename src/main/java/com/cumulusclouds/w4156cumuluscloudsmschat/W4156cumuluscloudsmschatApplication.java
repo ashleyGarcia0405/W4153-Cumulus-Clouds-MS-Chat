3203d5274c88eec533bcd0a04fe0b323ab091b2d
Ashley Garcia <ashley.garcia.official@gmail.com>
2024-10-30 22:46:49 -0400

    initial commit

---
+++ src/main/java/com/cumulusclouds/w4156cumuluscloudsmschat/W4156cumuluscloudsmschatApplication.java
@@ -0,0 +1,13 @@
+package com.cumulusclouds.w4156cumuluscloudsmschat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W4156cumuluscloudsmschatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W4156cumuluscloudsmschatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cumulusclouds/w4156cumuluscloudsmschat/W4156cumuluscloudsmschatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cumulusclouds.w4156cumuluscloudsmschat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W4156cumuluscloudsmschatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
